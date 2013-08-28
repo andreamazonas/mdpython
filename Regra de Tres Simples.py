d56e2b7211d4cc86c8f351d9512efb9eea5196f7
andreamazonas <andreamazonas@hotmail.com>
2013-08-28 19:01:39 -0300

    Create Regra de Tres Simples.py
    Mensagem de Erro : http://prntscr.com/1nzxsc

---
+++ Regra de Tres Simples.py
@@ -0,0 +1,20 @@
+#
+ #*        __  ___    __     ______                                  __  _           
+ #*       /  |/  /___/ /    / ____/___  _________  ____  _________ _/ /_(_)___  ____ 
+ #*      / /|_/ / __  /    / /   / __ \/ ___/ __ \/ __ \/ ___/ __ `/ __/ / __ \/ __ \
+ #*     / /  / / /_/ /    / /___/ /_/ / /  / /_/ / /_/ / /  / /_/ / /_/ / /_/ / / / /
+ #*    /_/  /_/\__,_(_)   \____/\____/_/  / .___/\____/_/   \__,_/\__/_/\____/_/ /_/ 
+ #*                                      /_/                                         
+ #*
+
+print ("Boa Noite, Md.")
+n1 = input( "Vamos precisar do Primeiro Numero")
+n2 = input( "Vamos precisar do Segundo Numero")
+n3 = input( "Vamos precisar do Terceiro Numero")
+n4 = x 
+n1*n3 = x0
+n2*n4 = x1
+x0/x1 = x3
+print ('Obrigado, Vamos calcular agora')
+print ('Calculando . . . Nao, nao da tempo de tomar um cafe!')
+print ('Essa eh facil o Valor de X eh,'x3)
