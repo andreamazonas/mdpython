fcd99c94f2f04c94addaf2ebbeb701ebfee9d7f8
andreamazonas <andreamazonas@hotmail.com>
2013-08-31 03:06:36 -0300

    Update Regra de Tres Simples.py

--- Regra de Tres Simples.py
+++ Regra de Tres Simples.py
@@ -10,11 +10,15 @@
  #*  n1 = n3
  #*
 
+
 print ("Boa Noite, Md.")
 n1 = input( "Vamos precisar do Primeiro Numero :")
 n2 = input( "Vamos precisar do Segundo Numero : ")
 n3 = input( "Vamos precisar do Terceiro Numero : ")
+n1 = int(n1)
+n2 = int(n2)
+n3 = int(n3)
 x = (n3*n2/n1)
 print ('Obrigado, Vamos calcular agora')
 print ('Calculando . . . Nao, nao da tempo de tomar um cafe!')
-print ('Essa eh facil o Valor de X eh : >>'),x
+print ('Essa eh facil o Valor de X é : ' + str(x))
