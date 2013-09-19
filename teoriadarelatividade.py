a10dc2ea58461a72427f6461c3d696b801af2ab4
andreamazonas <andreamazonas@hotmail.com>
2013-09-19 18:57:34 -0300

    Create teoriadarelatividade.py

---
+++ teoriadarelatividade.py
@@ -0,0 +1,22 @@
+print (" __  __     _      _____                                 _   _             ")
+print ("|  \/  |   | |    / ____|                               | | (_)            ")
+print ("| \  / | __| |   | |     ___  _ __ _ __   ___  _ __ __ _| |_ _  ___  _ __  ")
+print ("| |\/| |/ _` |   | |    / _ \| '__| '_ \ / _ \| '__/ _` | __| |/ _ \| '_ \ ")
+print ("| |  | | (_| |_  | |___| (_) | |  | |_) | (_) | | | (_| | |_| | (_) | | | |")
+print ("|_|  |_|\__,_(_)  \_____\___/|_|  | .__/ \___/|_|  \__,_|\__|_|\___/|_| |_|")
+print ("                                  | |                                      ")
+print ("                                  |_|                                      ")
+
+
+# Resolvendo a Equacao da Relatividade - Simples E=M.C2
+
+print ("Calcular E = Energia, a capacidade de realizar ou fazer")
+e = input (" Entre Valor da Energia:")
+e = int(e)
+print ("Calcular M = Massa, a quantidade de materia.")
+m = input (" Entre Valor da Massa:")
+m = int(m)
+print ("A velocidade da luz eh de 300000000 M/s, aguarde fazendo os calculos . . . ")
+c2 = 90000000000000000
+e1=m*c2/e
+print ("Valor da Energia eh:",e1)
