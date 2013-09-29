16519813fd893c7c454740d13e52e99cf64c12c0
andreamazonas <andreamazonas@hotmail.com>
2013-09-29 01:58:40 -0300

    Create Calculandoadistanciadeumraio.py
    Calculo simples para calcular a distancia de um raio com base no tempo que levar para ouvir o trovão.

---
+++ Calculandoadistanciadeumraio.py
@@ -0,0 +1,14 @@
+print (" __  __     _      _____                                 _   _             ")
+print ("|  \/  |   | |    / ____|                               | | (_)            ")
+print ("| \  / | __| |   | |     ___  _ __ _ __   ___  _ __ __ _| |_ _  ___  _ __  ")
+print ("| |\/| |/ _` |   | |    / _ \| '__| '_ \ / _ \| '__/ _` | __| |/ _ \| '_ \ ")
+print ("| |  | | (_| |_  | |___| (_) | |  | |_) | (_) | | | (_| | |_| | (_) | | | |")
+print ("|_|  |_|\__,_(_)  \_____\___/|_|  | .__/ \___/|_|  \__,_|\__|_|\___/|_| |_|")
+print ("                                  | |                                      ")
+
+drt = input( "Digite entre com o tempo entre o RAIO e o TROVAO:")
+vs = 340.29
+drt = float(drt)
+dr = drt * vs
+dr = float(dr)
+print (" A distancia em metros eh de :",dr)
